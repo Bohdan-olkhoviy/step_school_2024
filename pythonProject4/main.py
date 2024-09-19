b62ccdc2599ad984f6a23baacc3d522e566da7be
bohda <bohdan.olkhoviy@gmail.com>
2024-09-19 17:20:57 +0300

    added "from character import Character
    player1 = Character("Gojo", 100, 5, 0)
    player1.show_stats()
    
    player2 = Character("Sukuna", 100, 3, 2)
    player2.show_stats
    
    a = 3
    b = 5
    
    print(id(a))
    print(id(b))"

--- pythonProject4/main.py
+++ pythonProject4/main.py
@@ -1,37 +1,5 @@
-answer = 0
+from character import Character
 
-user = input('Яка країна найбільша? :')
-if user == 'Україна':
-    answer += 1
-    print('Правильно')
-else:
-    print("Неправильно")
-user = input('В якій країні зявилася китайська стіна :')
-if user == 'Китай':
-    answer += 1
-    print('Правильно')
-else:
-    print('Неправильно')
-
-user = input('Де ельфіва вежа :')
-if user == 'Франция':
-    answer += 1
-    print('Правильно')
-else:
-    print('Неправильно')
-
-user = input('Скільки Роблоку років :')
-if user == '37':
-    answer += 1
-    print("Правильно")
-else:
-    print("Неправильно")
-
-user = input("У якому місті Париж :")
-if user == 'Франция':
-    answer += 1
-    print('Правильно')
-else:
-    print('Неправильно')
-
-print("Правильних відповідей: ", answer)
+player1 = Character()
+player1.name = 'Sigma_Meawing'
+player1.health = 90
