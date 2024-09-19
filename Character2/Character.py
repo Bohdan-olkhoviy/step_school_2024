1c01d19fee5456be893ab57a215f0a1cf16f3af6
bohda <bohdan.olkhoviy@gmail.com>
2024-09-19 17:21:18 +0300

    added " class character:     name = ' '     Health = 100     damage = 5     Defendse = 90
        def __init__(self, name, health, damage, Defendse):
            self.name = name
            self.health = health
            self.damage = damage
            self.Defends = Defendse
    
            def show_stats(self):
                print(self)
    
            def __str__(self):
                return f"-- {self.name} -- \nЗдоровья: {self.health}\n" \
                      f"Шкода: {self.damage}\nЗахист: {self.defendse}"
    
                def take_damage(self, damage):"

---
+++ Character2/Character.py
@@ -0,0 +1,21 @@
+
+class character:
+    name = ' '
+    Health = 100
+    damage = 5
+    Defendse = 90
+
+    def __init__(self, name, health, damage, Defendse):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.Defends = Defendse
+
+        def show_stats(self):
+            print(self)
+
+        def __str__(self):
+            return f"-- {self.name} -- \nЗдоровья: {self.health}\n" \
+                  f"Шкода: {self.damage}\nЗахист: {self.defendse}"
+
+            def take_damage(self, damage):
