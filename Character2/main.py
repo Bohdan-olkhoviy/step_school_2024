b86ab64611739f93c33e76cfe1423d66e5868e8b
bohda <bohdan.olkhoviy@gmail.com>
2024-09-19 17:22:23 +0300

    added "from character import Character
    player1 = Character("Gojo", 100, 5, 0)
    player1.show_stats()
    
    player2 = Character("Sukuna", 100, 3, 2)
    player2.show_stats
    
    a = 3
    b = 5
    
    print(id(a))
    print(id(b))
    "
    
                def take_damage(self, damage):"

---
+++ Character2/main.py
@@ -0,0 +1,15 @@
+from character import Character
+
+player1 = Character("Gojo", 100, 5, 0)
+player1.show_stats()
+
+player2 = Character("Sukuna", 100, 3, 2)
+player2.show_stats
+
+
+
+a = 3
+b = 5
+
+print(id(a))
+print(id(b))
